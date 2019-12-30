e7aded594bbe4ce6950c1f5271967e9ef1f07ea5
Nicolas Cabrera <nicolascc1@hotmail.com>
2019-12-30 14:38:14 +0000

    Items added to list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ListItems from './ListItems';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Shopping List</h1>
-    </div>
-  );
+
+	const [items, setItems] = useState([]);
+	const [currentItem, setCurrentItem] = useState('');
+
+	function handleInput(e) {
+		setCurrentItem(e.target.value);
+	}
+
+	function addItem(e) {
+		e.preventDefault();
+		const newItem = currentItem;
+		if(newItem !== '') {
+			const newItems = [...items, newItem]; 
+			setItems(newItems);
+			setCurrentItem('');
+		}
+	}
+
+	return (
+		<div className="App">
+			<h1>Shopping List</h1>
+			<form onSubmit={(e) => addItem(e)}>
+				<input type='text'
+					placeholder='Enter item'
+					onChange={(e) => handleInput(e)}></input>
+				<button type='submit'>Add Item</button>
+			</form>
+			<label>{items}</label>
+			<ListItems/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/ListItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ListItems(props) {
+
+	const items = props.items;
+
+	return (
+		<div>
+			
+		</div>
+	)
+}
